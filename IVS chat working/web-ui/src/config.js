5eae2198866b3aa52dfed8b082e4cf63392a6c7f
Ayush-n25 <97076920+Ayush-n25@users.noreply.github.com>
2023-08-03 21:12:29 +0530

    Add files via upload

---
+++ IVS chat working/web-ui/src/config.js
@@ -0,0 +1,20 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/* eslint-disable */
+
+// Amazon IVS Playback URL
+// Replace this with your own Amazon IVS Playback URL
+export const PLAYBACK_URL = "https://760b256a3da8.us-east-1.playback.live-video.net/api/video/v1/us-east-1.049054135175.channel.6tM2Z9kY16nH.m3u8";
+
+// Chat websocket address
+// The AWS region that your room is created in. For example, `us-west-2`.
+export const CHAT_REGION = "us-east-1";
+
+// Chat API URL
+// The Amazon IVS Chat backend endpoint. You must deploy the serverless backend to get this value.
+// for running locally API_URL="http://127.0.0.1:3000"
+export const API_URL = "http://127.0.0.1:3000";
+
+// Chat room id (ARN)
+export const CHAT_ROOM_ID = "arn:aws:ivschat:us-east-1:128222158613:room/il381tflCyyv";
